b862dd3c62add9cc690e4f5ab217fd11627d507a
JoaquinxHerrera <joaquin.xherrera1@gmail.com>
2024-02-21 10:14:40 -0300

    new master

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -2,7 +2,7 @@ import "./styles.css";
 function Greeting (props) {
     return (
         <div className="greeting">
-            <p>Welcome to Uwear {props.username}! We are your personal wardrobe.</p>
+            <p>Welcome to Uwear {props.username}! We are your personal wardrobe.yaas</p>
         </div>
     )
 }
